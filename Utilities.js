177c951a5da373f7a10b062b8c250df3516699c4
Tristan Louie Pineda <tlcpineda.projects@gmail.com>
2026-01-01 01:34:32 +0800

    Update function documentation.

--- Utilities.js
+++ Utilities.js
@@ -1,13 +1,15 @@
 /**
  * Format the date/datetime object to ISO string format.
- * @param {string} date Date to be formatted, with year, month, and day values.
- * @param {string} time Time portion of the datetime object; defaults to start of day.
- * @param {string} tz IANA timezone; defaults to Zulu.
- * @returns {string} dt The formatted datetime string.
+ * @param {Object} datetime object with keys as :
+ * - 'date' (REQUIRED);
+ * - 'time' (OPTIONAL) defaults to '00:00:00.000'; and,
+ * - 'tz' (OPTIONAL) in IANA format; defaults to 'Zulu'.
+ *
+ * @returns {string} The formatted datetime string.
  */
-function format_as_iso(date, time = "00:00:00.000", tz = "Zulu") {
+function format_as_iso({ date, time = "00:00:00.000", tz = "Zulu" }) {
+  // Ignore provided time if, time is already included in date.
   if (!date.includes(":")) {
-    // Check if date does not includes time value; append time to date.
     date += ` ${time}`;
   }
 
