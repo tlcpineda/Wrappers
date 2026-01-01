1053b49102da4dc0626a0fb8d9bef891ad5070f6
Tristan Louie Pineda <tlcpineda.projects@gmail.com>
2026-01-01 11:39:46 +0800

    Include get_week_num.

--- Utilities.js
+++ Utilities.js
@@ -1,6 +1,6 @@
 /**
  * Format the date/datetime object to ISO string format.
- * @param {Object} datetime object with keys as :
+ * @param '' {Object} datetime object with keys as :
  * - 'date' {string} (REQUIRED);
  * - 'time' {string} (OPTIONAL) defaults to '00:00:00.000'; and,
  * - 'tz' {string} (OPTIONAL) in IANA format; defaults to 'Zulu'.
@@ -18,4 +18,15 @@ function format_as_iso({ date, time = "00:00:00.000", tz = "Zulu" }) {
     tz,
     "yyyy-MM-dd'T'HH:mm:ss.SSSXXX",
   );
+}
+
+/**
+ * Get the week number of the input date.
+ * @param date {string} The date for which the week number for the year is to be determined
+ * @param tz {string} The timezone associated with the date, in IANA format.
+ *
+ * @returns '' {string} The week number for the year.
+ */
+function get_week_num(date, tz = "Zulu") {
+  return Utilities.formatDate(date, tz, "w");
 }
